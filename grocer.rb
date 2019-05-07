8b8586d2cba7a4a811c20514cc73150eaa8d29f7
Kevin Dai <daikevin19@yahoo.com>
2019-05-07 21:07:17 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,30 @@
 def consolidate_cart(cart)
-  # code here
+  new_cart = {} # new hash
+  count = 0
+  cart.each do |element| #element is entire array
+    element.each do |fruit, hash| 
+      new_cart[fruit] ||= hash # new_cart[fruit] || new_cart[fruit] = hash
+      new_cart[fruit][:count] ||= 0 # initialize the count
+      new_cart[fruit][:count] += 1 # increase the count by one 
+      end
+    end
+  return new_cart 
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+ 
 end
 
 def apply_clearance(cart)
-  # code here
+  cart.each do |item, item_hash|
+    if item_hash[:clearance] # if clearance = true
+      new_price = item_hash[:price] * 0.80
+      item_hash[:price] = new_price.round(2)
+    end
+  end
+  cart
 end
 
 def checkout(cart, coupons)
-  # code here
+
 end
